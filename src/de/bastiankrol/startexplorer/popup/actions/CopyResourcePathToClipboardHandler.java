baf46ee7d2d487b2fe3bf59557762bc9d71ff9d2
basti1302 <bastian.krol@web.de>
2008-06-12 06:28:53 +0000

    Copy path to clipboard implemented

--- src/de/bastiankrol/startexplorer/popup/actions/CopyResourcePathToClipboardHandler.java
+++ src/de/bastiankrol/startexplorer/popup/actions/CopyResourcePathToClipboardHandler.java
@@ -2,8 +2,13 @@ package de.bastiankrol.startexplorer.popup.actions;
 
 import java.util.List;
 
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.widgets.Display;
+
 public class CopyResourcePathToClipboardHandler extends
-AbstractStartFromResourceHandler
+    AbstractStartFromResourceHandler
 {
   /**
    * {@inheritDoc}
@@ -14,7 +19,7 @@ AbstractStartFromResourceHandler
   {
     return PathCheck.ResourceType.BOTH;
   }
-  
+
   /**
    * {@inheritDoc}
    * 
@@ -23,6 +28,26 @@ AbstractStartFromResourceHandler
   @Override
   protected void doActionForResources(List<String> pathList)
   {
-    throw new RuntimeException("CopyResourcePathToClipboardHandler: Not implemented");
+    if (pathList.isEmpty())
+    {
+      return;
+    }
+    StringBuffer clipboardContentBuffer = new StringBuffer();
+    for (String path : pathList)
+    {
+      clipboardContentBuffer.append(path);
+      
+      // TODO Make this configurable via preference dialog, also allow line break
+      clipboardContentBuffer.append(", ");
+    }
+    String clipboardContent = clipboardContentBuffer.substring(0,
+        clipboardContentBuffer.length() - 2);
+    
+    Display display = Display.getDefault();
+    Clipboard clipboard = new Clipboard(display);
+    TextTransfer textTransfer = TextTransfer.getInstance();
+    clipboard.setContents(new Object[] { clipboardContent },
+        new Transfer[] { textTransfer });
+    clipboard.dispose();
   }
 }
